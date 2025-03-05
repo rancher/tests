c057a8465f6b993595ac6b1bb2852921e31a081b
Anupama <38144301+anupama2501@users.noreply.github.com>
2025-03-05 14:40:13 -0800

    Updating the test path

--- validation/pipeline/scripts/rancher_k8s_version.sh
+++ validation/pipeline/scripts/rancher_k8s_version.sh
@@ -3,7 +3,7 @@ set -e
 cd $(dirname $0)/../../../../../
 
 echo "Getting go rancher version "
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/rancherversion ./tests/v2/validation/pipeline/rancherversion
+env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o tests/v2/validation/rancherversion ./validation/pipeline/rancherversion
 
 echo "running rancher versions script to get the latest k8s versions"
 tests/v2/validation/rancherversion
