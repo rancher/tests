2254dfdfd5828745783de56d20d8be5c743dd5ba
Caleb Warren <slickwarren21@gmail.com>
2025-03-12 16:14:07 -0700

    check explicit rancher paths

--- .github/scripts/check-forked-repos.sh
+++ .github/scripts/check-forked-repos.sh
@@ -2,20 +2,20 @@
 
 set -e
 
-if grep -q "github.com/.*rancher/shepherd =>" go.mod; then
+if grep -q "github.com/rancher/shepherd =>" go.mod; then
     echo "Error: Forked version of rancher/shepherd detected in go.mod"
     exit 1
 fi
-if grep -q "github.com/.*rancher/shepherd =>" actions/go.mod; then
+if grep -q "github.com/rancher/shepherd =>" actions/go.mod; then
     echo "Error: Forked version of rancher/shepherd detected in actions/go.mod"
     exit 1
 fi
 
-if grep -q "github.com/.*rancher/rancher =>" go.mod; then
+if grep -q "github.com/rancher/rancher =>" go.mod; then
     echo "Error: Forked version of rancher/rancher detected in go.mod"
     exit 1
 fi
-if grep -q "github.com/.*rancher/rancher =>" actions/go.mod; then
+if grep -q "github.com/rancher/rancher =>" actions/go.mod; then
     echo "Error: Forked version of rancher/rancher detected in actions/go.mod"
     exit 1
 fi
