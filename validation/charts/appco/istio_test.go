9517f7ab93dbdebdf4f50a615afc38187d1a2caf
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2025-08-26 11:50:09 -0300

    Added pit.event tag

--- validation/charts/appco/istio_test.go
+++ validation/charts/appco/istio_test.go
@@ -1,4 +1,4 @@
-//go:build (validation || infra.any || cluster.any || extended) && !sanity && !stress
+//go:build (validation || infra.any || cluster.any || extended || pit.event) && !sanity && !stress
 
 package appco
 
