2d5d6d15454614187c9742084dab95f76d291c4f
Daniel Newman <dan@danlovesprogramming.com>
2025-09-26 07:45:10 -0500

    fix: specify backend type in OpenTofu initialization command

--- validation/pipeline/scripts/airgap_initialize_tofu.sh
+++ validation/pipeline/scripts/airgap_initialize_tofu.sh
@@ -10,7 +10,7 @@ export AWS_DEFAULT_REGION="${AWS_REGION:-us-east-2}"
 cd ${QA_INFRA_WORK_PATH}
 
 echo 'Initializing OpenTofu with S3 backend configuration...'
-tofu -chdir=tofu/aws/modules/airgap init -backend-config="${TERRAFORM_BACKEND_VARS_FILENAME}" -input=false -upgrade
+tofu -chdir=tofu/aws/modules/airgap init -backend=s3 -backend-config="${TERRAFORM_BACKEND_VARS_FILENAME}" -input=false -upgrade
 
 echo 'Verifying initialization success...'
 tofu -chdir=tofu/aws/modules/airgap providers
