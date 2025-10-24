a7514d597199bf9f49fd0641b89cd18e1a2e0f7e
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2025-10-24 17:52:04 -0300

    Added elemental ansible command

--- validation/pipeline/scripts/build_elemental_qa_infra.sh
+++ validation/pipeline/scripts/build_elemental_qa_infra.sh
@@ -7,14 +7,17 @@ echo "Create elemental infra"
 : "${ELEMENTAL_TFVARS_FILE:=elemental.tfvars}"
 : "${ELEMENTAL_TOFU_PATH:=tofu/gcp/modules/elemental_nodes}"
 : "${QAINFRA_SCRIPT_PATH:=/root/go/src/github.com/rancher/qa-infra-automation}"
+: "${ELEMENTAL_PLAYBOOK_PATH:=ansible/rancher/downstream/elemental}"
+: "${ELEMENTAL_PLAYBOOK_FILE:=elemental-playbook.yml}"
+: "${ELEMENTAL_VARS_FILE:=vars.yaml}"
 
-cd "$QAINFRA_SCRIPT_PATH"
+cd "$QAINFRA_SCRIPT_PATH/$ELEMENTAL_TOFU_PATH"
 
-tofu -chdir="$ELEMENTAL_TOFU_PATH" init
-tofu -chdir="$ELEMENTAL_TOFU_PATH" apply -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
+tofu init
+tofu apply -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
 if [ $? -ne 0 ] && [[ $CLEANUP == "true" ]]; then
     echo "Error: Playbook failed."
-    tofu -chdir="$ELEMENTAL_TOFU_PATH" destroy -auto-approve -var-file="$ELEMENTAL_TFVARS_FILE"
+    tofu destroy -auto-approve -var-file="$ELEMENTAL_TFVARS_FILE"
     if [ $? -ne 0 ]; then
         echo "Error: Terraform destroy failed."
         exit 1
@@ -23,3 +26,10 @@ if [ $? -ne 0 ] && [[ $CLEANUP == "true" ]]; then
     exit 1
 fi
 
+chmod 600 private_key.pem
+
+export ELEMENTAL_NODE_IP=$(tofu output -raw public_dns)
+
+cd "$QAINFRA_SCRIPT_PATH/$ELEMENTAL_PLAYBOOK_PATH"
+
+ansible-playbook "$ELEMENTAL_PLAYBOOK_FILE" -vvvv -e "@$ELEMENTAL_VARS_FILE" --extra-vars "elemental_node_public_ip=$ELEMENTAL_NODE_IP"
