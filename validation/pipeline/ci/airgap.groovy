c63a4bf2a997c9a8b50beca1384bc108aa8ee244
Daniel Newman <dan@danlovesprogramming.com>
2025-11-06 21:52:13 -0600

    Hopefully fixed artifact archiving

--- validation/pipeline/ci/airgap.groovy
+++ validation/pipeline/ci/airgap.groovy
@@ -386,11 +386,11 @@ def extractArtifactsFromDockerVolume(ctx) {
                 elif [ -f /source/ansible-inventory.yml ]; then
                     cp /source/ansible-inventory.yml /dest/
                 fi
-                [ -f "/source/${TERRAFORM_VARS_FILENAME}" ] && cp "/source/${TERRAFORM_VARS_FILENAME}" /dest/ || true
+                [ -f "/source/\$TERRAFORM_VARS_FILENAME" ] && cp "/source/\$TERRAFORM_VARS_FILENAME" /dest/ || true
                 [ -f /source/terraform.tfstate ] && cp /source/terraform.tfstate /dest/ || true
                 [ -f /source/terraform-state-primary.tfstate ] && cp /source/terraform-state-primary.tfstate /dest/ || true
                 for f in /source/terraform-state-backup-*.tfstate /source/tfstate-backup-*.tfstate; do
-                    [ -f "$f" ] && cp "$f" /dest/ || true
+                    [ -f "\$f" ] && cp "\$f" /dest/ || true
                 done
                 if [ -f /source/kubeconfig.yaml ]; then
                     cp /source/kubeconfig.yaml /dest/
