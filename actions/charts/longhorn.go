74c4afe3668f47554d935adfbd7a9dd24266be76
hamistao <pedro.ribeiro@suse.com>
2025-11-19 10:57:24 -0300

    Add Longhorn actions
    Signed-off-by: hamistao <pedro.ribeiro@suse.com>

---
+++ actions/charts/longhorn.go
@@ -0,0 +1,137 @@
+package charts
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/rancher/shepherd/clients/rancher"
+	"github.com/rancher/shepherd/clients/rancher/catalog"
+	shepherdCharts "github.com/rancher/shepherd/extensions/charts"
+	"github.com/rancher/shepherd/extensions/defaults"
+	"github.com/rancher/shepherd/pkg/api/steve/catalog/types"
+	"github.com/rancher/shepherd/pkg/wait"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+)
+
+var (
+	longhornChartName     = "longhorn"
+	enableDeletionSetting = map[string]any{
+		"defaultSettings": map[string]any{
+			"deletingConfirmationFlag": true,
+		},
+	}
+)
+
+type LonghornGlobalSettingPut struct {
+	Links map[string]string `json:"links"`
+	ID    string            `json:"id"`
+	Name  string            `json:"name"`
+	Type  string            `json:"type"`
+	Value string            `json:"value"`
+}
+
+// InstallLonghornChart installs ther Longhorn chart on the cluster related to the received catalog client object.
+// Enable the wait parameter if you wish for this function to wait for all deployments and daemonsets to be ready before returning.
+func InstallLonghornChart(client *rancher.Client, payload PayloadOpts, values map[string]interface{}) error {
+	catalogClient, err := client.GetClusterCatalogClient(payload.Cluster.ID)
+	if err != nil {
+		return err
+	}
+
+	// If no specific value for setting deletingConfirmationFlag was provided, default to have it enabled so cleanup works as extected.
+	if values == nil {
+		values = enableDeletionSetting
+	} else {
+		defaultSettings, ok := values["defaultSettings"].(map[string]any)
+		if !ok {
+			return errors.New(`Provided values map has invalid value for "defaultSettings"`)
+		}
+
+		_, ok = defaultSettings["deletingConfirmationFlag"]
+		if !ok {
+			defaultSettings["deletingConfirmationFlag"] = true
+		}
+	}
+
+	chartInstalls := []types.ChartInstall{
+		*NewChartInstall(longhornChartName+"-crd", payload.Version, payload.Cluster.ID, payload.Cluster.Name, payload.Host, catalog.RancherChartRepo, payload.ProjectID, payload.DefaultRegistry, nil),
+		*NewChartInstall(longhornChartName, payload.Version, payload.Cluster.ID, payload.Cluster.Name, payload.Host, catalog.RancherChartRepo, payload.ProjectID, payload.DefaultRegistry, values),
+	}
+
+	chartInstallAction := NewChartInstallAction(payload.Namespace, payload.ProjectID, chartInstalls)
+
+	err = catalogClient.InstallChart(chartInstallAction, catalog.RancherChartRepo)
+	if err != nil {
+		return err
+	}
+
+	client.Session.RegisterCleanupFunc(func() error {
+		chart, err := shepherdCharts.GetChartStatus(client, payload.Cluster.ID, payload.Namespace, longhornChartName)
+		if err != nil || !chart.IsAlreadyInstalled {
+			return err
+		} else {
+			return UninstallLonghornChart(client, payload.Namespace, payload.Cluster.ID, payload.Host)
+		}
+	})
+
+	err = shepherdCharts.WaitChartInstall(catalogClient, payload.Namespace, longhornChartName)
+	if err != nil {
+		return err
+	}
+
+	err = shepherdCharts.WatchAndWaitDeployments(client, payload.Cluster.ID, payload.Namespace, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	err = shepherdCharts.WatchAndWaitDaemonSets(client, payload.Cluster.ID, payload.Namespace, metav1.ListOptions{})
+	return err
+}
+
+// UninstallLonghornChart removes Longhorn from the cluster related to the received catalog client object.
+func UninstallLonghornChart(client *rancher.Client, namespace string, clusterID string, rancherHostname string) error {
+	catalogClient, err := client.GetClusterCatalogClient(clusterID)
+	if err != nil {
+		return err
+	}
+
+	err = catalogClient.UninstallChart(longhornChartName, namespace, NewChartUninstallAction())
+	if err != nil {
+		return err
+	}
+
+	err = waitUninstallation(catalogClient, namespace, longhornChartName)
+	if err != nil {
+		return err
+	}
+
+	// Uninstall CRDs last so we still have them in case uninstalling longhorn fails as they help debugging.
+	err = catalogClient.UninstallChart(longhornChartName+"-crd", namespace, NewChartUninstallAction())
+	if err != nil {
+		return err
+	}
+
+	return waitUninstallation(catalogClient, namespace, longhornChartName+"-crd")
+}
+
+func waitUninstallation(catalogClient *catalog.Client, namespace string, chartName string) error {
+	watchAppInterface, err := catalogClient.Apps(namespace).Watch(context.TODO(), metav1.ListOptions{
+		FieldSelector:  "metadata.name=" + chartName,
+		TimeoutSeconds: &defaults.WatchTimeoutSeconds,
+	})
+	if err != nil {
+		return err
+	}
+
+	return wait.WatchWait(watchAppInterface, func(event watch.Event) (ready bool, err error) {
+		switch event.Type {
+		case watch.Error:
+			return false, fmt.Errorf("there was an error uninstalling Longhorn chart")
+		case watch.Deleted:
+			return true, nil
+		}
+		return false, nil
+	})
+}
