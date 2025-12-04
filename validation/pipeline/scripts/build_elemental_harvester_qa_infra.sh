8d710bbf59851bea348415193f3baa3d2861c8ee
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2025-12-04 12:06:44 -0300

    feat: Added tofu command to create the elemental-vms

--- validation/pipeline/scripts/build_elemental_harvester_qa_infra.sh
+++ validation/pipeline/scripts/build_elemental_harvester_qa_infra.sh
@@ -5,13 +5,14 @@ echo "Create elemental harvester infra"
 
 : "${CLEANUP:=true}"
 : "${ELEMENTAL_TFVARS_FILE:=elemental.tfvars}"
-: "${ELEMENTAL_TOFU_PATH:=tofu/aws/modules/s3}"
+: "${S3_TOFU_PATH:=tofu/aws/modules/s3}"
 : "${QAINFRA_SCRIPT_PATH:=/root/go/src/github.com/rancher/qa-infra-automation}"
 : "${ELEMENTAL_PLAYBOOK_PATH:=ansible/rancher/downstream/elemental/harvester}"
 : "${ELEMENTAL_PLAYBOOK_FILE:=elemental-playbook.yml}"
 : "${ELEMENTAL_VARS_FILE:=vars.yaml}"
+: "${HARVESTER_TOFU_PATH:=tofu/harvester/modules/elemental-vm}"
 
-cd "$QAINFRA_SCRIPT_PATH/$ELEMENTAL_TOFU_PATH"
+cd "$QAINFRA_SCRIPT_PATH/$S3_TOFU_PATH"
 
 tofu init
 tofu apply -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
@@ -19,13 +20,28 @@ if [ $? -ne 0 ] && [[ $CLEANUP == "true" ]]; then
     echo "Error: Playbook failed."
     tofu destroy -auto-approve -var-file="$ELEMENTAL_TFVARS_FILE"
     if [ $? -ne 0 ]; then
-        echo "Error: Terraform destroy failed."
+        echo "Error: Tofu destroy failed."
         exit 1
     fi
-    echo "Terraform infrastructure destroyed successfully!"
+    echo "Tofu infrastructure destroyed successfully!"
     exit 1
 fi
 
 cd "$QAINFRA_SCRIPT_PATH/$ELEMENTAL_PLAYBOOK_PATH"
 
 ansible-playbook "$ELEMENTAL_PLAYBOOK_FILE" -e "@$ELEMENTAL_VARS_FILE"
+
+cd "$QAINFRA_SCRIPT_PATH/$HARVESTER_TOFU_PATH"
+
+tofu init
+tofu apply -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
+if [ $? -ne 0 ] && [[ $CLEANUP == "true" ]]; then
+    echo "Error: Playbook failed."
+    tofu destroy -auto-approve -var-file="$ELEMENTAL_TFVARS_FILE"
+    if [ $? -ne 0 ]; then
+        echo "Error: Tofu destroy failed."
+        exit 1
+    fi
+    echo "Tofu infrastructure destroyed successfully!"
+    exit 1
+fi
--- validation/pipeline/scripts/destroy_elemental_harvester_qa_infra.sh
+++ validation/pipeline/scripts/destroy_elemental_harvester_qa_infra.sh
@@ -4,10 +4,13 @@ set -ex
 echo "Destroy elemental harvester infra"
 
 : "${ELEMENTAL_TFVARS_FILE:=elemental.tfvars}"
-: "${ELEMENTAL_TOFU_PATH:=tofu/aws/modules/s3}"
+: "${S3_TOFU_PATH:=tofu/aws/modules/s3}"
 : "${QAINFRA_SCRIPT_PATH:=/root/go/src/github.com/rancher/qa-infra-automation}"
+: "${HARVESTER_TOFU_PATH:=tofu/harvester/modules/elemental-vm}"
 
 cd "$QAINFRA_SCRIPT_PATH"
 
-tofu -chdir="$ELEMENTAL_TOFU_PATH" destroy -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
+tofu -chdir="$S3_TOFU_PATH" destroy -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
+
+tofu -chdir="$HARVESTER_TOFU_PATH" destroy -auto-approve -var-file=$ELEMENTAL_TFVARS_FILE
 
