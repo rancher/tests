0c0c58dc7184b781cba787ceb5e96256866215f0
Leonardo Scalabrini <leonardo.scalabrini@suse.com>
2026-01-13 16:14:05 -0300

    feat: Added NeuVector Monitor chart installation test

--- actions/charts/neuvector.go
+++ actions/charts/neuvector.go
@@ -9,21 +9,25 @@ import (
 )
 
 const (
-	NeuVectorNamespace = "neuvector-system"
-	NeuVectorChartName = "neuvector"
+	NeuVectorNamespace        = "neuvector-system"
+	NeuVectorChartName        = "neuvector"
+	NeuVectorMonitorChartName = "neuvector-monitor"
 )
 
 // InstallNeuVectorChart installs the NeuVector chart on the cluster according to data on the payload.
 // This also waits for installation to complete and checks if the deployments are Ready.
-func InstallNeuVectorChart(client *rancher.Client, payload PayloadOpts) error {
+func InstallNeuVectorChart(client *rancher.Client, neuVectorChartName string, payload PayloadOpts) error {
 	catalogClient, err := client.GetClusterCatalogClient(payload.Cluster.ID)
 	if err != nil {
 		return err
 	}
 
 	chartInstalls := []types.ChartInstall{
-		*NewChartInstall(NeuVectorChartName+"-crd", payload.Version, payload.Cluster.ID, payload.Cluster.Name, payload.Host, catalog.RancherChartRepo, payload.ProjectID, payload.DefaultRegistry, nil),
-		*NewChartInstall(NeuVectorChartName, payload.Version, payload.Cluster.ID, payload.Cluster.Name, payload.Host, catalog.RancherChartRepo, payload.ProjectID, payload.DefaultRegistry, nil),
+		*NewChartInstall(neuVectorChartName, payload.Version, payload.Cluster.ID, payload.Cluster.Name, payload.Host, catalog.RancherChartRepo, payload.ProjectID, payload.DefaultRegistry, nil),
+	}
+
+	if neuVectorChartName == NeuVectorChartName {
+		chartInstalls = append(chartInstalls, *NewChartInstall(neuVectorChartName+"-crd", payload.Version, payload.Cluster.ID, payload.Cluster.Name, payload.Host, catalog.RancherChartRepo, payload.ProjectID, payload.DefaultRegistry, nil))
 	}
 
 	chartInstallAction := NewChartInstallAction(payload.Namespace, payload.ProjectID, chartInstalls)
@@ -34,10 +38,10 @@ func InstallNeuVectorChart(client *rancher.Client, payload PayloadOpts) error {
 	}
 
 	client.Session.RegisterCleanupFunc(func() error {
-		return UninstallNeuVectorChart(client, payload.Namespace, payload.Cluster.ID, payload.Host)
+		return UninstallNeuVectorChart(client, neuVectorChartName, payload.Namespace, payload.Cluster.ID, payload.Host)
 	})
 
-	err = shepherdCharts.WaitChartInstall(catalogClient, payload.Namespace, NeuVectorChartName)
+	err = shepherdCharts.WaitChartInstall(catalogClient, payload.Namespace, neuVectorChartName)
 	if err != nil {
 		return err
 	}
@@ -52,26 +56,30 @@ func InstallNeuVectorChart(client *rancher.Client, payload PayloadOpts) error {
 }
 
 // UninstallNeuVectorChart removes NeuVector from the cluster related to the received catalog client object.
-func UninstallNeuVectorChart(client *rancher.Client, namespace string, clusterID string, rancherHostname string) error {
+func UninstallNeuVectorChart(client *rancher.Client, neuVectorChartName string, namespace string, clusterID string, rancherHostname string) error {
 	catalogClient, err := client.GetClusterCatalogClient(clusterID)
 	if err != nil {
 		return err
 	}
 
-	err = catalogClient.UninstallChart(NeuVectorChartName, namespace, NewChartUninstallAction())
+	err = catalogClient.UninstallChart(neuVectorChartName, namespace, NewChartUninstallAction())
 	if err != nil {
 		return err
 	}
 
-	err = waitUninstallation(catalogClient, namespace, NeuVectorChartName)
+	err = waitUninstallation(catalogClient, namespace, neuVectorChartName)
 	if err != nil {
 		return err
 	}
 
-	err = catalogClient.UninstallChart(NeuVectorChartName+"-crd", namespace, NewChartUninstallAction())
-	if err != nil {
-		return err
+	if neuVectorChartName == NeuVectorChartName {
+		err = catalogClient.UninstallChart(neuVectorChartName+"-crd", namespace, NewChartUninstallAction())
+		if err != nil {
+			return err
+		}
+
+		return waitUninstallation(catalogClient, namespace, neuVectorChartName+"-crd")
 	}
 
-	return waitUninstallation(catalogClient, namespace, NeuVectorChartName+"-crd")
+	return nil
 }
