7033a7c262082ce9347656e5c95a042658fbef10
hamistao <pedro.ribeiro@suse.com>
2026-02-20 11:14:50 -0300

    Avoid using Errorf needlessly
    Signed-off-by: hamistao <pedro.ribeiro@suse.com>

--- interoperability/fleet/fleet.go
+++ interoperability/fleet/fleet.go
@@ -157,7 +157,7 @@ func VerifyGitRepo(client *rancher.Client, gitRepoID, k8sClusterID, steveCluster
 	podErrors := pods.StatusPods(client, k8sClusterID)
 	if len(podErrors) > 0 {
 		for _, err := range podErrors {
-			logrus.Errorf(err.Error())
+			logrus.Error(err.Error())
 		}
 		return errors.New("pods are not healthy in " + steveClusterID)
 	}
