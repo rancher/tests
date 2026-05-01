5b6e7710c36c77263d909a698f1c9db01cbfac81
Markus Walker <markus.walker.25@gmail.com>
2026-05-01 10:14:36 -0700

    Add automation to register SCC code to Prime UI checks

--- validation/prime/prime_test.go
+++ validation/prime/prime_test.go
@@ -9,10 +9,9 @@ import (
 	"github.com/rancher/shepherd/clients/rancher"
 	"github.com/rancher/shepherd/extensions/defaults/namespaces"
 	"github.com/rancher/shepherd/extensions/defaults/stevetypes"
-	"github.com/rancher/shepherd/extensions/rancherversion"
 	"github.com/rancher/shepherd/pkg/config"
 	"github.com/rancher/shepherd/pkg/session"
-	prime "github.com/rancher/tests/actions/prime"
+	"github.com/rancher/tests/actions/prime"
 	"github.com/rancher/tests/actions/provisioning"
 	"github.com/rancher/tests/actions/qase"
 	"github.com/rancher/tests/actions/workloads/pods"
@@ -23,6 +22,7 @@ import (
 )
 
 const (
+	sccNamespace   = "cattle-scc-system"
 	systemRegistry = "system-default-registry"
 	localCluster   = "local"
 	uiBrand        = "ui-brand"
@@ -31,112 +31,32 @@ const (
 
 type PrimeTestSuite struct {
 	suite.Suite
-	session        *session.Session
-	cattleConfig   map[string]any
-	client         *rancher.Client
-	brand          string
-	isPrime        bool
-	rancherVersion string
-	primeRegistry  string
+	session      *session.Session
+	cattleConfig map[string]any
+	client       *rancher.Client
+	primeConfig  *prime.Config
 }
 
-func (t *PrimeTestSuite) TearDownSuite() {
-	t.session.Cleanup()
+func (p *PrimeTestSuite) TearDownSuite() {
+	p.session.Cleanup()
 }
 
-func (t *PrimeTestSuite) SetupSuite() {
+func (p *PrimeTestSuite) SetupSuite() {
 	testSession := session.NewSession()
-	t.session = testSession
+	p.session = testSession
 
-	t.cattleConfig = config.LoadConfigFromFile(os.Getenv(config.ConfigEnvironmentKey))
+	p.cattleConfig = config.LoadConfigFromFile(os.Getenv(config.ConfigEnvironmentKey))
 
-	primeConfig := new(rancherversion.Config)
-	config.LoadConfig(rancherversion.ConfigurationFileKey, primeConfig)
+	p.primeConfig = new(prime.Config)
+	config.LoadConfig(prime.ConfigurationFileKey, p.primeConfig)
 
-	t.brand = primeConfig.Brand
-	t.isPrime = primeConfig.IsPrime
-	t.rancherVersion = primeConfig.RancherVersion
-	t.primeRegistry = primeConfig.Registry
+	client, err := rancher.NewClient("", p.session)
+	assert.NoError(p.T(), err)
 
-	client, err := rancher.NewClient("", t.session)
-	assert.NoError(t.T(), err)
-
-	t.client = client
+	p.client = client
 }
 
-func (t *PrimeTestSuite) TestPrimeBrand() {
-	tests := []struct {
-		name string
-	}{
-		{"Prime_Brand"},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func() {
-			rancherBrand, err := t.client.Management.Setting.ByID(uiBrand)
-			require.NoError(t.T(), err)
-
-			checkBrand := prime.CheckUIBrand(t.client, t.isPrime, rancherBrand, t.brand)
-			assert.NoError(t.T(), checkBrand)
-		})
-
-		params := provisioning.GetProvisioningSchemaParams(t.client, t.cattleConfig)
-		err := qase.UpdateSchemaParameters(tt.name, params)
-		if err != nil {
-			logrus.Warningf("Failed to upload schema parameters %s", err)
-		}
-	}
-}
-
-func (t *PrimeTestSuite) TestPrimeVersion() {
-	tests := []struct {
-		name string
-	}{
-		{"Prime_Version"},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func() {
-			serverVersion, err := t.client.Management.Setting.ByID(serverVersion)
-			require.NoError(t.T(), err)
-
-			checkVersion := prime.CheckVersion(t.isPrime, t.rancherVersion, serverVersion)
-			assert.NoError(t.T(), checkVersion)
-		})
-
-		params := provisioning.GetProvisioningSchemaParams(t.client, t.cattleConfig)
-		err := qase.UpdateSchemaParameters(tt.name, params)
-		if err != nil {
-			logrus.Warningf("Failed to upload schema parameters %s", err)
-		}
-	}
-}
-
-func (t *PrimeTestSuite) TestSystemDefaultRegistry() {
-	tests := []struct {
-		name string
-	}{
-		{"Prime_System_Default_Registry"},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func() {
-			registry, err := t.client.Management.Setting.ByID(systemRegistry)
-			require.NoError(t.T(), err)
-
-			checkRegistry := prime.CheckSystemDefaultRegistry(t.isPrime, t.primeRegistry, registry)
-			assert.NoError(t.T(), checkRegistry)
-		})
-
-		params := provisioning.GetProvisioningSchemaParams(t.client, t.cattleConfig)
-		err := qase.UpdateSchemaParameters(tt.name, params)
-		if err != nil {
-			logrus.Warningf("Failed to upload schema parameters %s", err)
-		}
-	}
-}
-
-func (t *PrimeTestSuite) TestLocalClusterRancherImages() {
+func (p *PrimeTestSuite) TestLocalClusterRancherImages() {
 	tests := []struct {
 		name string
 	}{
@@ -144,15 +64,99 @@ func (t *PrimeTestSuite) TestLocalClusterRancherImages() {
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func() {
-			cluster, err := t.client.Steve.SteveType(stevetypes.Provisioning).ByID(namespaces.FleetLocal + "/" + localCluster)
-			require.NoError(t.T(), err)
+		p.T().Run(tt.name, func(t *testing.T) {
+			cluster, err := p.client.Steve.SteveType(stevetypes.Provisioning).ByID(namespaces.FleetLocal + "/" + localCluster)
+			require.NoError(p.T(), err)
 
-			err = pods.VerifyClusterPods(t.client, cluster)
-			require.NoError(t.T(), err)
+			err = pods.VerifyClusterPods(p.client, cluster)
+			require.NoError(p.T(), err)
 		})
 
-		params := provisioning.GetProvisioningSchemaParams(t.client, t.cattleConfig)
+		params := provisioning.GetProvisioningSchemaParams(p.client, p.cattleConfig)
+		err := qase.UpdateSchemaParameters(tt.name, params)
+		if err != nil {
+			logrus.Warningf("Failed to upload schema parameters %s", err)
+		}
+	}
+}
+
+func (p *PrimeTestSuite) TestPrimeBrand() {
+	tests := []struct {
+		name string
+	}{
+		{"Prime_Brand"},
+	}
+
+	for _, tt := range tests {
+		p.T().Run(tt.name, func(t *testing.T) {
+			logrus.Infof("Verifying Rancher Prime brand is: %s", p.primeConfig.Brand)
+			rancherBrand, err := p.client.Management.Setting.ByID(uiBrand)
+			require.NoError(p.T(), err)
+			require.Equal(p.T(), p.primeConfig.Brand, rancherBrand.Value)
+		})
+
+		params := provisioning.GetProvisioningSchemaParams(p.client, p.cattleConfig)
+		err := qase.UpdateSchemaParameters(tt.name, params)
+		if err != nil {
+			logrus.Warningf("Failed to upload schema parameters %s", err)
+		}
+	}
+}
+
+func (p *PrimeTestSuite) TestSCCRegistration() {
+	tests := []struct {
+		name string
+	}{
+		{"Prime_SCC_Registration"},
+	}
+
+	for _, tt := range tests {
+		p.T().Run(tt.name, func(t *testing.T) {
+			secret, err := prime.CreateSCCRegistrationSecret(sccNamespace, p.primeConfig.SCCRegistrationCode, p.primeConfig.SCCRegistrationType)
+			require.NoError(p.T(), err)
+
+			sccSecret, err := p.client.Steve.SteveType("secret").Create(secret)
+			require.NoError(p.T(), err)
+
+			logrus.Infof("Verifying SCC registration exists in namespace: %s", sccNamespace)
+			sccRegistration, err := p.client.Steve.SteveType("scc.cattle.io.registration").ListAll(nil)
+			require.NoError(p.T(), err)
+			require.NotEmpty(p.T(), sccRegistration)
+
+			logrus.Infof("Deleting SCC registration secret in namespace: %s", sccNamespace)
+			err = p.client.Steve.SteveType("secret").Delete(sccSecret)
+			require.NoError(p.T(), err)
+
+			logrus.Infof("Verifying SCC registration is deleted in namespace: %s", sccNamespace)
+			_, err = p.client.Steve.SteveType("scc.cattle.io.registration").ListAll(nil)
+			require.NoError(p.T(), err)
+			require.Empty(p.T(), err)
+		})
+
+		params := provisioning.GetProvisioningSchemaParams(p.client, p.cattleConfig)
+		err := qase.UpdateSchemaParameters(tt.name, params)
+		if err != nil {
+			logrus.Warningf("Failed to upload schema parameters %s", err)
+		}
+	}
+}
+
+func (p *PrimeTestSuite) TestSystemDefaultRegistry() {
+	tests := []struct {
+		name string
+	}{
+		{"Prime_System_Default_Registry"},
+	}
+
+	for _, tt := range tests {
+		p.T().Run(tt.name, func(t *testing.T) {
+			logrus.Infof("Verifying system default registry is set to: %s", p.primeConfig.Registry)
+			registry, err := p.client.Management.Setting.ByID(systemRegistry)
+			require.NoError(p.T(), err)
+			require.Equal(p.T(), p.primeConfig.Registry, registry.Value)
+		})
+
+		params := provisioning.GetProvisioningSchemaParams(p.client, p.cattleConfig)
 		err := qase.UpdateSchemaParameters(tt.name, params)
 		if err != nil {
 			logrus.Warningf("Failed to upload schema parameters %s", err)
