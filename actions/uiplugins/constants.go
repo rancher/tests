b926aa156eb60a9618d7021198e621d720060ba9
Daniel Newman <dan@danlovesprogramming.com>
2026-08-11 20:52:00 -0500

    Fix NeuVector test cleanup hang and error spam (#818)

--- actions/uiplugins/constants.go
+++ actions/uiplugins/constants.go
@@ -1,16 +1,8 @@
 package uiplugins
 
-import (
-	"github.com/rancher/shepherd/extensions/defaults"
-)
-
 const (
 	harvesterExtensionsName  = "harvester"
 	stackstateExtensionsName = "observability"
 	stackstatePluginName     = "rancher-ui-plugins"
 	local                    = "local"
-)
-
-var (
-	timeoutSeconds = int64(defaults.TwoMinuteTimeout)
 )
