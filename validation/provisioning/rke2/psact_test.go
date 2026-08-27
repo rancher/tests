0c4fd63c8389bb12462de6951f1b87a6756380b7
Sam Gartner <samuel.gartner@suse.com>
2026-08-27 11:09:15 -0500

    Integrate secrets manager into cluster provisioning to source credentials securely during workflow execution (#843)

--- validation/provisioning/rke2/psact_test.go
+++ validation/provisioning/rke2/psact_test.go
@@ -49,6 +49,12 @@ func psactSetup(t *testing.T) psactTest {
 	r.cattleConfig, err = defaults.LoadPackageDefaults(r.cattleConfig, "")
 	require.NoError(t, err)
 
+	r.cattleConfig, err = defaults.LoadSecretsManagerDefaults(r.cattleConfig)
+	require.NoError(t, err)
+
+	err = defaults.VerifyCattleConfig(r.cattleConfig, nil)
+	require.NoError(t, err)
+
 	loggingConfig := new(logging.Logging)
 	operations.LoadObjectFromMap(logging.LoggingKey, r.cattleConfig, loggingConfig)
 
