c3ea2e830824814efc6c8d1acdae83c1d3241217
Joe Sims <joseph.sims@suse.com>
2026-08-28 14:14:10 -0500

    Added ext session tokens to ext token test suite (#860)

--- actions/kubeapi/tokens/exttokens/verify.go
+++ actions/kubeapi/tokens/exttokens/verify.go
@@ -53,6 +53,33 @@ func VerifyExtTokenData(client *rancher.Client, exttoken *extapi.Token, expected
 	return nil
 }
 
+// VerifyExtSessionTokenData validates that a session ext token has the expected properties
+func VerifyExtSessionTokenData(exttoken *extapi.Token, expectedUserID string) error {
+	userID, ok := exttoken.Labels[UserIDLabel]
+	if !ok {
+		return fmt.Errorf("expected label cattle.io/user-id to exist on token")
+	}
+	if userID != expectedUserID {
+		return fmt.Errorf("label mismatch for user ID: got '%s', want '%s'", userID, expectedUserID)
+	}
+	if exttoken.Spec.Kind != "session" {
+		return fmt.Errorf("expected Spec.Kind to be 'session', got '%s'", exttoken.Spec.Kind)
+	}
+	if exttoken.Spec.Enabled == nil {
+		return fmt.Errorf("Spec.Enabled should not be nil")
+	}
+	if !*exttoken.Spec.Enabled {
+		return fmt.Errorf("expected session token to be enabled, but it was disabled")
+	}
+	if exttoken.Status.Value == "" {
+		return fmt.Errorf("expected Status.Value to be populated, but it was empty")
+	}
+	if exttoken.Status.Hash != "" {
+		return fmt.Errorf("expected Status.Hash to be empty on creation, but it was: '%s'", exttoken.Status.Hash)
+	}
+	return nil
+}
+
 // VerifyExtTokenExistsInList returns true if a token with the given name exists in the provided list
 func VerifyExtTokenExistsInList(tokens []extapi.Token, name string) bool {
 	for _, token := range tokens {
