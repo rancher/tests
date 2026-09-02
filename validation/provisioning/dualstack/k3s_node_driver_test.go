348be3f089342fb33a5becd88ae74e313b1c46b4
Sam Gartner <samuel.gartner@suse.com>
2026-09-02 16:05:24 -0500

    Integrate secrets manager into dualstack cluster provisioning tests and workflow to source credentials securely
    fix
    
    Update defaults.yaml
    
    fixes
    
    Update dualstack-cluster-provisioning.yml
    
    fix
    
    fix
    
    fix
    
    fix
    
    Update dualstack-cluster-provisioning.yml
    
    Update dualstack-cluster-provisioning.yml
    
    Update dualstack-cluster-provisioning.yml
    
    fix
    
    fix
    
    fix

--- validation/provisioning/dualstack/k3s_node_driver_test.go
+++ validation/provisioning/dualstack/k3s_node_driver_test.go
@@ -46,6 +46,12 @@ func nodeDriverK3SSetup(t *testing.T) nodeDriverK3STest {
 	k.cattleConfig, err = defaults.LoadPackageDefaults(k.cattleConfig, "")
 	require.NoError(t, err)
 
+	k.cattleConfig, err = defaults.LoadSecretsManagerDefaults(k.cattleConfig)
+	require.NoError(t, err)
+
+	err = defaults.VerifyCattleConfig(k.cattleConfig, nil)
+	require.NoError(t, err)
+
 	loggingConfig := new(logging.Logging)
 	operations.LoadObjectFromMap(logging.LoggingKey, k.cattleConfig, loggingConfig)
 
