45d6683d78c053acbcca98c1884c93ecb47b48b5
Sam Gartner <samuel.gartner@suse.com>
2026-09-25 16:43:05 -0500

    Enhance proxy provisioning workflow and tests to use aws secrets manager (#947)

--- validation/provisioning/proxy/k3s_custom_test.go
+++ validation/provisioning/proxy/k3s_custom_test.go
@@ -50,6 +50,12 @@ func customK3SProxySetup(t *testing.T) customK3SProxyTest {
 	k.cattleConfig, err = defaults.LoadPackageDefaults(k.cattleConfig, "")
 	require.NoError(t, err)
 
+	k.cattleConfig, err = defaults.LoadSecretsManagerDefaults(k.cattleConfig)
+	require.NoError(t, err)
+
+	err = defaults.VerifyCattleConfig(k.cattleConfig, nil)
+	require.NoError(t, err)
+
 	loggingConfig := new(logging.Logging)
 	operations.LoadObjectFromMap(logging.LoggingKey, k.cattleConfig, loggingConfig)
 
